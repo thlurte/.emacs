75d80ae6e046e762c62ae1211bf445e1da6e63ab
thlurte <thlurte@gmail.com>
2023-12-06 18:07:21 +0530

    added org mode

--- init.el
+++ init.el
@@ -1,45 +1,69 @@
+;; Basic UI Configuration ------------------------------------------------------
+
 (setq inhibit-startup-message t)
-
-
 (scroll-bar-mode -1) ; Disable visible scroll
 (tool-bar-mode -1)   ; Disable the toolbar
 (tooltip-mode -1)    ; Disable tooltips
 (set-fringe-mode 10) ; More space
 
-
-(menu-bar-mode -1)  ; Disable the menu bar
 ;; setup the visible bell
 (setq visible-bell t)
 
+(menu-bar-mode -1)  ; Disable the menu bar
 
-(set-face-attribute 'default nil :font "Hack Nerd Font" :height 90)
+;; Font Configuration --------------------------------------------------------
+
+(set-face-attribute 'default nil :font "Hack Nerd Font" :height 95)
+
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+;; Keyboard Configuration ----------------------------------------------------
 
 
 ;; Make ESC quit prompts
+;;(use-package command-log-mode)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+(add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+;; hmm this is kinda fun
+(global-set-key (kbd "C-M-s")'counsel-switch-buffer)
+(global-set-key (kbd "C-M-l")'counsel-switch-to-shell-buffer)
+(global-set-key (kbd "M-q")'previous-buffer)
+(global-set-key (kbd "M-e")'next-buffer)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+;; Package Manager Configuration ---------------------------------------------
 
 ;; Initialize package sources
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 		 ("melpa-stable" . "https://stable.melpa.org/packages/")
                         ("org" . "https://orgmode.org/elpa/")
                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
 (package-initialize)
 (unless package-archive-contents
  (package-refresh-contents))
 
-  ;; Initialize use-package on non-Linux platforms
+;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
  (package-install 'use-package))
+
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
-(use-package command-log-mode)
-
+;; Ivy Configuration --------------------------------------------------------
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -58,38 +82,13 @@
   :config
   (ivy-mode 1))
 
- (use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom (doom-modeline-height 5))
-o
- (setq doom-modeline-major-mode-icon t)
-
-
- (column-number-mode)
-
-
- (global-display-line-numbers-mode t)
-
-
-
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
- (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
+
+
+
+;; Counsel Configuration ---------------------------------------------------
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
@@ -101,16 +100,14 @@ o
   (setq ivy-initial-inputs-alist nil))
 
 
-(use-package helpful
-  :ensure t
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+;; Doom packages for UI Configuration --------------------------------------
+
+(use-package doom-modeline
+ :ensure t
+ :init (doom-modeline-mode 1)
+ :custom (doom-modeline-height 25))
+
+(setq doom-modeline-major-mode-icon t)
 
 (use-package doom-themes
   :ensure t
@@ -130,6 +127,26 @@ o
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+
+
+;; Other packages --------------------------------------------------------
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 (use-package all-the-icons
   :if (display-graphic-p)
   :commands all-the-icons-install-fonts
@@ -142,11 +159,9 @@ o
   :hook (dired-mode . all-the-icons-dired-mode))
 
 
-;; hmm this is kinda fun
-(global-set-key (kbd "C-M-s")'counsel-switch-buffer)
-(global-set-key (kbd "C-M-l")'counsel-switch-to-shell-buffer)
-(global-set-key (kbd "M-q")'previous-buffer)
-(global-set-key (kbd "M-e")'next-buffer)
+
+
+;; Evil packages configurations -----------------------------------------
 
 (use-package evil
   :init
@@ -171,6 +186,8 @@ o
   :config
   (evil-collection-init))
 
+
+
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
@@ -184,14 +201,76 @@ o
 
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
+
+
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-(use-package evil-magit
-  :after magit)
 
 ;; NOTE: Make sure to configure a GitHub token before using this package!
 ;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
 ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
 (use-package forge)
+
+
+;; Org Mode Configuration --------------------------------------------------
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+(setq org-startup-with-inline-images t)
+
+;; use-package with package.el:
+(use-package dashboard
+  :ensure t
+  :init
+  (progn
+    (setq dashboard-items '((recents  . 5)
+                        (projects . 5)
+                        (agenda . 5)))
+    (setq dashboard-startup-banner "~/lab/.emacs/pi.txt"))
+  :config
+  (dashboard-open))
