68069c8ffd0f4a208f0e12229c4cde47e39b3724
thlurte <thlurte@gmail.com>
2024-01-21 23:16:46 +0530

    20240121231719679898

--- init.el
+++ init.el
@@ -350,7 +350,13 @@
            (file+olp+datetree "~/lab/nucleus/agenda/entries.org")
            "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
            :clock-in :clock-resume
-           :empty-lines 1)))
+           :empty-lines 1)
+      ("jw" "Work Journl" entry
+           (file+olp+datetree "~/lab/nucleus/agenda/wjournals.org")
+           "* %<%I:%M %p> - %a :workjournal:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
+      ))
 
   (define-key global-map (kbd "C-c j")
     (lambda () (interactive) (org-capture nil "jj")))
@@ -635,23 +641,77 @@
     "H" 'dired-hide-dotfiles-mode))
 
 
+(defun efs/exwm-update-class ()
+  (exwm-workspace-rename-buffer exwm-class-name))
+
+(use-package exwm
+  :config
+  ;; Set the default number of workspaces
+  (setq exwm-workspace-number 5)
+
+  ;; When window "class" updates, use it to set the buffer name
+  ;; (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
+
+  ;; These keys should always pass through to Emacs
+  (setq exwm-input-prefix-keys
+    '(?\C-x
+      ?\C-u
+      ?\C-h
+      ?\M-x
+      ?\M-`
+      ?\M-&
+      ?\M-:
+      ?\C-\M-j  ;; Buffer list
+      ?\C-\ ))  ;; Ctrl+Space
+
+  ;; Ctrl+Q will enable the next key to be sent directly
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+  ;; Set up global key bindings.  These always work, no matter the input state!
+  ;; Keep in mind that changing this list after EXWM initializes has no effect.
+  (setq exwm-input-global-keys
+        `(
+          ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+          ([?\s-r] . exwm-reset)
+
+          ;; Move between windows
+          ([s-left] . windmove-left)
+          ([s-right] . windmove-right)
+          ([s-up] . windmove-up)
+          ([s-down] . windmove-down)
+
+          ;; Launch applications via shell command
+          ([?\s-&] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))
+
+          ;; Switch workspace
+          ([?\s-w] . exwm-workspace-switch)
+
+          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
+
+  (exwm-enable))
+
 (use-package dashboard
   :ensure t
+  :config
+  (dashboard-setup-startup-hook)
   :init
   (progn
     (setq dashboard-items '((recents  . 5)
                             (projects . 5)
                             (agenda . 5)
-                            (custom . dashboard-insert-custom))) ; Add the custom item
+                            )) ; Add the custom item
     ;;(setq dashboard-center-content t)
     (setq dashboard-set-footer nil)
     (setq dashboard-init-info "")
     (setq dashboard-banner-logo-title "thlurte")
     (setq dashboard-startup-banner "~/lab/.emacs/pi.txt"))
-  :config
-  (defun dashboard-insert-custom (list-size)
-    (insert "Library:\n")
-    (dolist (file (directory-files "/home/ahmed/library" t "\\.epub$"))
-      (insert (format "  %s\n" (file-name-nondirectory file)))))
-  
-  (dashboard-open))
+  )
+
